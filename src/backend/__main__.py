007594981568e5b7514085d4e5958cef071cf6bc
HippoProgrammer <bcgcustomerservices@gmail.com>
2025-07-05 09:47:40 +0100

    Adding Postgres connection

--- src/backend/__main__.py
+++ src/backend/__main__.py
@@ -1,13 +1,16 @@
 # Import modules
 import yaml
 import logging
-from secrets import token_urlsafe as rand_token
-import os
-import subprocess
 import time
+import psycopg
 
 # Create a new logging object
 log = logging.getLogger(__name__)
 
 with open('/run/secrets/db_pass') as file:
     db_pass = file.read()
+
+with psycopg.connect(f"postgres://library:{password}@db:5432/library") as conn: # create a connection to the db
+    with conn.cursor() as cur: # open a cursor
+        pass # ADD SQL COMMANDS HERE
+        conn.commit()
