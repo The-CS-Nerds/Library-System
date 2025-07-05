1f40443a1ee97750f53c2838ba34d4bb6a17ed95
HippoProgrammer <bcgcustomerservices@gmail.com>
2025-07-05 08:58:07 +0100

    Testing

--- src/backend/__main__.py
+++ src/backend/__main__.py
@@ -1,6 +1,31 @@
 # Import modules
 import yaml
 import logging
+from secrets import token_urlsafe as rand_token
+import os
+import subprocess
+import time
 
 # Create a new logging object
 log = logging.getLogger(__name__)
+
+cwd = os.getcwd()
+
+host = os.environ['DB_HOST']
+port = os.environ['DB_PORT']
+database = '*'
+username = '*'
+
+
+# Write a pgpass file
+with open(f'{cwd}/.pgpass','w') as file:
+    file.write(f'{host}:{port}:{database}:{username}:{rand_token(512)}')
+subprocess.run(f'chmod 0600 {cwd}/.pgpass')
+os.environ['PGPASSFILE'] = f'{cwd}/.pgpass'
+
+while True:
+    try:
+        subprocess.run('pg_isready -U library')
+        break
+    except subprocess.CalledProcessError:
+        time.sleep(5)
