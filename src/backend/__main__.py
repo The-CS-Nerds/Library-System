36dc78c1a397951983c12f8cd2462d6c34d223f0
HippoProgrammer <bcgcustomerservices@gmail.com>
2025-07-05 09:48:04 +0100

    Minor bug fix

--- src/backend/__main__.py
+++ src/backend/__main__.py
@@ -10,7 +10,7 @@ log = logging.getLogger(__name__)
 with open('/run/secrets/db_pass') as file:
     db_pass = file.read()
 
-with psycopg.connect(f"postgres://library:{password}@db:5432/library") as conn: # create a connection to the db
+with psycopg.connect(f"postgres://library:{db_pass}@db:5432/library") as conn: # create a connection to the db
     with conn.cursor() as cur: # open a cursor
         pass # ADD SQL COMMANDS HERE
         conn.commit()
