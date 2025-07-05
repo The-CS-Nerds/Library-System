f4fe8f271187f30806514852acf4e2676dce453c
HippoProgrammer <bcgcustomerservices@gmail.com>
2025-07-05 15:12:56 +0100

    Fixed logging

--- src/backend/backend.Dockerfile
+++ src/backend/backend.Dockerfile
@@ -3,10 +3,10 @@ FROM python:3-alpine3.21
 WORKDIR /src
 
 COPY requirements.txt requirements.txt
-RUN pip install --no-cache-dir --require-hashes -r requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
 
 RUN adduser --disabled-password --gecos '' appuser \
- && chown -R appuser /app
+ && chown -R appuser /src
 USER appuser
 
 ADD ./* ./
--- src/frontend/__main__.py
+++ src/frontend/__main__.py
@@ -1,14 +1,29 @@
 # Import modules
 from flask import Flask
-import logging
+import logging, sys
 
 # Create a new logging object
 log = logging.getLogger(__name__)
+log_handler = logging.StreamHandler(sys.stdout)
 
-# Create a flask app
-app = Flask(__name__)
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+log_handler.setFormatter(formatter)
 
-# Create a home page
-@app.route("/")
-def index():
-    return "<p>Under Construction</p>"
+log.addHandler(log_handler)
+log.setLevel(20)
+
+if __name__ == "__main__":
+    log.info('Creating Flask app...')
+
+    # Create a flask app
+    app = Flask(__name__)
+    log.info('Created Flask app.')
+
+    log.info('Creating page decorators...')
+    # Create a home page
+    @app.route("/")
+    def index():
+        return "<p>Under Construction</p>"
+
+    log.info('Created page decorators.')
+    log.info('Frontend exited.')
