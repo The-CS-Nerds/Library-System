d844fe6a3d6072b23657cf806c63ed09de931870
SuitablyMysterious <fly2be@outlook.com>
2025-07-07 16:43:02 +0100

    fix `docker-entrypoint.sh` to use correct db name (I hope)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,6 +8,6 @@ export CASBIN_LOGIN_PASS="$(< /run/secrets/casbin_login_pass)"
 
 export DB_HOST="${DB_HOST:-localhost}"
 export DB_PORT="${DB_PORT:-5432}"
-export DB_NAME="${DB_NAME:-library}"
+export DB_NAME="${DB_NAME:-library-system}"
 
 exec /usr/local/bin/docker-entrypoint.sh "$@"
