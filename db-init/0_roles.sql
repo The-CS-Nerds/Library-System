c40e1fdd5065d71f8f1174495c7c3b03619d94ff
HippoProgrammer <bcgcustomerservices@gmail.com>
2025-07-07 19:16:06 +0100

    Update all licenses to comply with AGPL standards.

--- db-init/0_roles.sql
+++ db-init/0_roles.sql
@@ -1,16 +1,19 @@
-/* This repository is made for the purpose of managing online library systems
-Copyright (C) <2025>  <The CS Nerds>
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as published
-by the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-This is program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
-You should have received a copy of the GNU Affero General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>. */
+/* 
+#    This program is for the automatic creation of database roles upon first start.
+#    Copyright (C) 2025, The CS Nerds (HippoProgrammer & SuitablyMysterious)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.*/
 
 
 -- Create casbin role
