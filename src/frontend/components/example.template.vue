6cf7939f3c959a0dbeb633b177e7dc4a1c58205a
SuitablyMysterious <fly2be@outlook.com>
2025-07-13 08:27:06 +0100

    Create basic Vue structure

---
+++ src/frontend/components/example.template.vue
@@ -0,0 +1,11 @@
+<template>
+<!-- Put the HTML for this element here -->
+</template>
+
+<script>
+// Here place the js code for this element
+</script>
+
+<style scoped>
+/* This is css for the specific component, leave blank if using global styles */
+</style>
