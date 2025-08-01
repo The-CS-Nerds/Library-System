f5fee50d6254c1fbd3feb9b558303b55f687540a
Suitably Mysterious <fly2be@outlook.com>
2025-08-01 18:10:10 +0000

    add `-d` flag to `launch.sh`

--- launch.sh
+++ launch.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export DB_PASS=`python3 dbKeyGen.py --keys db --print`
 export CASBIN_LOGIN_PASS=`python3 dbKeyGen.py --keys auth --print`
-docker compose up --build
+docker compose up --build -d
 unset DB_PASS
 unset CASBIN_LOGIN_PASS
