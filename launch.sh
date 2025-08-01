17a651cc98ddadffdb9ee14cbfc184a4fe045d22
Suitably Mysterious <fly2be@outlook.com>
2025-08-01 18:23:41 +0000

    add a --kill argument to launch.sh

--- launch.sh
+++ launch.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 export DB_PASS=`python3 dbKeyGen.py --keys db --print`
 export CASBIN_LOGIN_PASS=`python3 dbKeyGen.py --keys auth --print`
-docker compose up --build $1
+if [ $1 = "--kill" ]; then
+  docker compose down
+  docker system prune
+  exit 0
+fi
+else;
+  docker compose up --build $1
+fi
 unset DB_PASS
 unset CASBIN_LOGIN_PASS
