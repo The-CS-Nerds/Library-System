4d537fa9506f9d28b44cd7bf2c7ad83403f1d118
Suitably Mysterious <fly2be@outlook.com>
2025-08-02 15:30:52 +0000

    covenience change to `launch.sh`

--- launch.sh
+++ launch.sh
@@ -2,8 +2,9 @@
 export DB_PASS=`python3 dbKeyGen.py --keys db --print`
 export CASBIN_LOGIN_PASS=`python3 dbKeyGen.py --keys auth --print`
 if [ $1 = "--kill" ]; then
-  docker compose down
+  docker compose down -v
   docker system prune --force
+  docker volume prune --force
   exit 0
 else
   docker compose up --build $1 backend db
