b4ea574ce0a69475e8e19cd91d1aaa913964a121
Suitably Mysterious <fly2be@outlook.com>
2025-08-02 13:03:53 +0000

    Fix #140

--- src/backend/__main__.py
+++ src/backend/__main__.py
@@ -55,4 +55,4 @@ api.add_resource(getBookByName, '/Book/getByName')
 api.add_resource(getBookByID, '/Book/getByID')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='localhost', port=8000)
